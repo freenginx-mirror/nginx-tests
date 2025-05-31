9aa2f449bfd32f1a4c598dd5106adec827970d73
Maxim Dounin <mdounin@mdounin.ru>
2025-06-01 01:12:21 +0300

    Tests: fixed Valgrind errors filtering.
    Broken in 1995:d329b05e20fa during $t->grep_file() introduction and
    resulted in warnings in output (and no filtering) when testing with
    TEST_NGINX_VALGRIND set.

--- lib/Test/Nginx.pm
+++ lib/Test/Nginx.pm
@@ -89,7 +89,7 @@ sub DESTROY {
 	}
 
 	if (Test::More->builder->expected_tests && $ENV{TEST_NGINX_VALGRIND}) {
-		my $errors = $self->grep_file('valgrind.log', /^==\d+== .+/m);
+		my $errors = $self->grep_file('valgrind.log', qr/^==\d+== .+/m);
 		Test::More::is($errors, '', 'no valgrind errors');
 	}
 
